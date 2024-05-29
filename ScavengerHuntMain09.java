cddcc95f4b4b1fcc0af1f53b4eb9698de7eb14cf
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:34:13 +0700

    ADD PERTEMUAN 11 (tugas)

---
+++ ScavengerHuntMain09.java
@@ -0,0 +1,10 @@
+public class ScavengerHuntMain09 {
+    public static void main(String[] args) {
+        ScavengerHunt09 game = new ScavengerHunt09();
+        game.addPoint("Apa ibu kota Jawa Timur?", "Surabaya");
+        game.addPoint("Gunung tertinggi di dunia?", "Everest");
+        game.addPoint("Mata uang negara Jepang?", "Yen");
+        
+        game.startGame();
+    }
+}
