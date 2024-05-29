1c8898746737d6b4f768ce168bbde902b32b4075
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:13:50 +0700

    ADD PERTEMUAN 7

---
+++ Mahasiswa09.java
@@ -0,0 +1,19 @@
+public class Mahasiswa09 {
+    int nim;
+    String nama;
+    int umur;
+    double ipk;
+
+    Mahasiswa09(int ni, String n, int u, double i){
+        nim = ni;
+        nama = n;
+        umur = u;
+        ipk = i;
+    }
+    void tampil(){
+        System.out.println("Nim  = " + nim);
+        System.out.println("Nama = " + nama);
+        System.out.println("Umur = " + umur);
+        System.out.println("IPK  = " + ipk);
+    }
+}
