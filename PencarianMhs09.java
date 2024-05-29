d848bbba9ee22c13a63b915e9d6967cc28b8c6d6
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:15:53 +0700

    ADD PERTEMUAN 7

---
+++ PencarianMhs09.java
@@ -0,0 +1,105 @@
+public class PencarianMhs09 {
+    Mahasiswa09 listMhs[] = new Mahasiswa09[5];
+    int idx;
+
+    public PencarianMhs09(int jumMhs){
+        listMhs = new Mahasiswa09[jumMhs];
+    }
+
+    void tambah(Mahasiswa09 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh !!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa09 m : listMhs) {
+            m.tampil();
+            System.out.println("---------------------------------------------");
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].nim == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    public void TampilPosisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data\t : " + x + " ditemukan pada index " + pos);
+        } else {
+            System.out.println("Data\t : " + x + " tidak ditemukan");
+        }
+    }
+    public void TampilData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Nim\t : " + x);
+            System.out.println("Nama\t : " + listMhs[pos].nama);
+            System.out.println("Umur\t : " + listMhs[pos].umur);
+            System.out.println("IPK\t : " + listMhs[pos].ipk);
+        } else {
+            System.out.println("Data\t : " + x + " tidak ditemukan");
+        }
+    }
+
+    public void TampilposisiNama(String cari, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + cari + "ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + cari + "tidak ditemukan");
+        }
+    }
+
+    public void TampilDataNama(String x, int pos) {
+        if (pos != -1) {
+            System.out.println("Nim\t  : " + x);
+            System.out.println("Nama\t : " + listMhs[pos].nama);
+            System.out.println("Umur\t : " + listMhs[pos].umur);
+            System.out.println("IPK\t  : " + listMhs[pos].ipk);
+        } else {
+            System.out.println("data " + x + "tidak ditemukan");
+        }
+    }
+    
+    public int FindBinarySearch(String cari, int left, int right){
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            int comparison = listMhs[mid].nama.compareTo(cari);
+            
+            if (comparison < 0) {
+                return FindBinarySearch(cari, mid + 1, right);
+            } else if (comparison < 0) {
+                return FindBinarySearch(cari, left, mid - 1);
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    public void countSearchName(String cari){
+        int count = 0;
+        for (int i=0; i<listMhs.length; i++) {
+            if (cari.equalsIgnoreCase(listMhs[i].nama)) {
+                count += 1;
+            }
+        }
+        if (count > 1) {
+            System.out.println("Data yang ditemukan lebih dari 1");
+            return;
+        }
+    }
+
+    public int FindBinarySearchName(String cari, int i, int j, int k) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'FindBinarySearchName'");
+    }
+}
