742f6e7baa096f39644916e5f6ee181d1f653053
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:26:29 +0700

    ADD PERTEMUAN 10

---
+++ Nasabah09.java
@@ -0,0 +1,19 @@
+public class Nasabah09 {
+    String norek;
+    String nama;
+    String alamat;
+    int umur;
+    double saldo;
+
+    public Nasabah09(String norek, String nama, String alamat, int umur, double saldo){
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+        
+    }
+    Nasabah09(){
+        
+    }
+}
