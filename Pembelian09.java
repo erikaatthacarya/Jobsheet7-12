8e8811657d5011214e9c5a3e8adcb8c54e1af171
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:25:11 +0700

    ADD PERTEMUAN 10

---
+++ Pembelian09.java
@@ -0,0 +1,18 @@
+public class Pembelian09 {
+    String nama;
+    int no_ID;
+    int umur;
+    char jk;
+
+    Pembelian09 (String nama, int no_ID, char jk, int umur){
+        this.nama = nama;
+        this.no_ID = no_ID;
+        this.jk = jk;
+        this.umur = umur;
+    }
+
+    Pembelian09(){
+
+    }
+}
+    
