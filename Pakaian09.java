6282f84a15101af3a946fbc62aa8ba3be54d753b
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:19:36 +0700

    ADD PERTEMUAN 8

---
+++ Pakaian09.java
@@ -0,0 +1,13 @@
+public class Pakaian09 {
+    String jenis, warna, merk, ukuran;
+    double harga;
+
+    Pakaian09(String jenis, String warna, String merk, String ukuran, double harga){
+        this.jenis = jenis;
+        this.warna = warna;
+        this.merk = merk;
+        this.ukuran = ukuran;
+        this.harga = harga;
+    }
+}
+    
