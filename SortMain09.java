8a02e326f7d1e490c69070d50538bc7439bb3821
Erika <143980757+erikaatthacarya@users.noreply.github.com>
2024-05-29 22:17:06 +0700

    ADD PERTEMUAN 7

---
+++ SortMain09.java
@@ -0,0 +1,12 @@
+public class SortMain09 {
+    public static void main(String[] args) {
+        int data[] = { 10, 40, 30, 50, 70, 20, 100, 90 };
+        System.out.println("Sorting dengan merge sort");
+        MergeSort09 mSort = new MergeSort09();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
